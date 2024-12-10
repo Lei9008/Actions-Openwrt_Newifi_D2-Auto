2a6562c950fa81d3d08d8da482f39930a7a2f77d
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-10 18:19:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,7 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
+# Modify default IP 修改后台管理IP地址
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 # 删除老argon
 rm -rf package/lean/luci-theme-argon
